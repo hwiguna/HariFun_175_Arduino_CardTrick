cbda61216afc296d2c1dc5a45c60575dfa3fafe1
Hari Wiguna <hwiguna@gmail.com>
2019-03-10 22:43:11 -0500

    Fix the missing 10 cards

---
+++ 06_BlackMagic03/06_BlackMagic03.ino
@@ -0,0 +1,112 @@
+// Black Magic Card Trick by Hari Wiguna, 2019
+// Modify for AdaFruit I2C LCD backpack because the LCD that comes with the kit is not working.
+// Fixing the missing 10 rank cards.
+
+// The secret:
+// The position of the first black suit card indicates
+// the position of where the chosen card would appear in next batch.
+
+//-- LCD Variables --
+#include "Wire.h"
+#include "Adafruit_LiquidCrystal.h"
+Adafruit_LiquidCrystal lcd(0);
+
+//-- BUTTON --
+byte button = 4; // Digital pin 4
+
+//-- Magic trick classes --
+#include "Pile.h"
+#include "Magician.h"
+
+void WaitForButtonPress() {
+  while (digitalRead(4) != LOW) {
+    delay(100);
+  }
+  delay(500); // ignore switch bounce
+}
+
+void ReadyToDeal() {
+    //         123456789*123456
+  lcd.print(" Ready to deal  ");
+  lcd.setCursor(0,1);
+  lcd.print("out the cards...");
+}
+
+Card PickACard(Pile deck) {
+  Card card = blankCard;
+  bool buttonPressed = false;
+  int prevCardIndex = -1;
+  while (!buttonPressed) {
+    int potValue = analogRead(A0);
+    int cardIndex = map(potValue, 0, 1023, 0, 51);
+    if (cardIndex != prevCardIndex) { // Only refresh screen if chosen card changes
+      prevCardIndex = cardIndex;
+      card = deck.PeekCard(cardIndex);
+      lcd.clear();
+      lcd.print("Pick a card...");
+      lcd.setCursor(0,1);
+      lcd.print(card.CardToString());
+    }
+
+    if (digitalRead(button) == LOW) {
+      buttonPressed = true;
+      delay(200); // ignore any button bounce noise
+    }
+    delay(50);
+  }
+
+  //-- Confirm chosen card --
+  lcd.clear();
+  lcd.print("You've chosen");
+  lcd.setCursor(0,1);
+  lcd.print(card.CardToString());
+
+  return card;
+}
+
+void SetupLCD() {
+  lcd.begin(16, 2); // 16 columns, 2 rows
+  lcd.clear();
+  lcd.print("* Magic Trick *");
+  lcd.setCursor(0,1);
+  lcd.print(" by Hari Wiguna "); 
+  lcd.setBacklight(HIGH);
+  delay(1500);
+}
+
+void SetupInputs() {
+  pinMode(button, INPUT_PULLUP);
+}
+
+void setup() {
+  SetupLCD();
+  Serial.begin(115200);
+  SetupInputs();
+
+  randomSeed(analogRead(A1)); // Get random seed from an unused Analog pin.
+  //-- Unit Testing --
+//  Pile testDeck = Pile(); // Unlike in C#, you don't say new Class.
+//  testDeck.UnitTest();
+}
+
+
+void loop() {
+  //-- Prepare deck --
+  Pile deck = Pile(); // Unlike in C#, you don't say new Class.
+  deck.InitFullDeck();
+
+  //-- Let user pick a card --
+  Card chosenCard = PickACard(deck); // Let user pick a card
+  WaitForButtonPress();
+
+  //-- Shuffle the deck --
+  deck.Shuffle();
+  ReadyToDeal();
+  WaitForButtonPress();
+
+  //-- Perform Magic! --
+  Magician shinLim = Magician(deck);
+  shinLim.SetChosenCard( chosenCard );
+  shinLim.PerformMagic();
+}
+
---
+++ 06_BlackMagic03/Magician.h
@@ -0,0 +1,93 @@
+
+class Magician {
+  public:
+    Magician(Pile deck);
+    SetChosenCard(Card chosenCard);
+    PerformMagic();
+  private:
+    Card _chosenCard;
+    Pile _deck;
+};
+
+//=== IMPLEMENTATION ===
+
+Magician::Magician(Pile deck) {
+  _deck = deck;
+  _chosenCard = blankCard;
+}
+
+Magician::SetChosenCard(Card chosenCard) {
+  _chosenCard = chosenCard;
+  byte index = _deck.RemoveCard(_chosenCard);
+  Serial.print("\nRemoved chosen card from location ");
+  Serial.println(index);
+}
+
+void WaitForButtonPress2() {
+  while (digitalRead(4) != LOW) {
+    delay(100);
+  }
+  delay(500); // ignore switch bounce
+}
+
+Magician::PerformMagic() {
+  Serial.println("\nWas it one of these cards?");
+  bool hasShownMarker = false;
+  bool hasShownChosenCard = false;
+  bool isOutOfCards = false;
+
+  while (!isOutOfCards) {
+    Card card1 = blankCard;
+    Card card2 = blankCard;
+
+    if (!hasShownMarker) { // Have we shown marker yet? NO
+      bool isTimeForMarker = random(5) == 4L; // 0,1,2,3,4. So 1 in 5 chance we should show marker card now
+      if (isTimeForMarker) { // Is it time to show marker? YES
+        bool showChosenOnSamePage = random(3) == 2L; // 0,1,2. So, 1 in 3 chance we should show chosen card on same page as marker card or on next page
+        if (showChosenOnSamePage) {
+          card1 =  _deck.TakeMarkerCard(); // Show marker card...
+          card2 = _chosenCard; // immediately follow with chosen card on same page.
+          hasShownChosenCard = true;
+        } else {
+          card1 =  _deck.TakeNonMarkerCard(); // show random non-marker card
+          card2 =  _deck.TakeMarkerCard(); // show marker card to indicate that first card on next page is chosen card.
+        }
+        hasShownMarker = true; // either way, we would have shown marker.
+      }
+      else { // Not time to show marker yet, so just show two random cards.
+        card1 =  _deck.TakeNonMarkerCard(); // show random non-marker card
+        card2 =  _deck.TakeNonMarkerCard(); // show random non-marker card
+      }
+    }
+    else { // We have shown marker on previous page, so it's time to show chosen card
+      if (!hasShownChosenCard) {
+        card1  = _chosenCard;
+        card2 =  _deck.TakeNonMarkerCard(); // show random non-marker card
+        hasShownChosenCard = true;
+      } else {
+        // The show is over, but let them keep spitting out remaining cards
+        card1 =  _deck.RemoveTopCard();
+        card2 =  _deck.RemoveTopCard();
+      }
+    }
+
+    lcd.clear();
+    if (card1.Value() == 0) {
+      //         123456789*123456
+      lcd.print(" No more cards! ");
+      isOutOfCards = true;
+    } else {
+      lcd.print(card1.CardToString());
+      lcd.setCursor(0, 1);
+      lcd.print(card2.CardToString());
+      //      card1.Print();
+      //      card2.Print();
+      //      Serial.println();
+    }
+
+    WaitForButtonPress2();
+  }
+
+  Serial.println("Take a bow!\n");
+}
+
